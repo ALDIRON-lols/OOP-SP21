53fdb9c95f6e95f354921359a801d0a38bb5ee94
ALDIRON-lols <80191450+ALDIRON-lols@users.noreply.github.com>
2021-04-28 00:15:09 +0500

    Delete Lab Reports/Question 3 directory

--- Lab Reports/Question 3/GraduateStudent.java
+++
@@ -1,9 +0,0 @@
-
-package Lab_9_Assignment_1_Task_3;
-
-public class GradStudent extends Student{
-    
-    public void take_exam(){
-        System.out.println("Please take your final exam.");
-    }
-}
--- Lab Reports/Question 3/PhDStudent.java
+++
@@ -1,9 +0,0 @@
-
-package Lab_9_Assignment_1_Task_3;
-
-public class PhdStudent extends Student{
-    
-    public void take_exam(){
-        System.out.println("Please provide your final Defense Presentation.");
-    }
-}
