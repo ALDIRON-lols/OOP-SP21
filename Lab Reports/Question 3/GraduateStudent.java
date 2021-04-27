67fa75ab324f6f70f93910479818b9aa6db719ea
ALDIRON-lols <80191450+ALDIRON-lols@users.noreply.github.com>
2021-04-28 00:09:13 +0500

    Add files via upload

---
+++ Lab Reports/Question 3/GraduateStudent.java
@@ -0,0 +1,9 @@
+
+package Lab_9_Assignment_1_Task_3;
+
+public class GradStudent extends Student{
+    
+    public void take_exam(){
+        System.out.println("Please take your final exam.");
+    }
+}
---
+++ Lab Reports/Question 3/PhDStudent.java
@@ -0,0 +1,9 @@
+
+package Lab_9_Assignment_1_Task_3;
+
+public class PhdStudent extends Student{
+    
+    public void take_exam(){
+        System.out.println("Please provide your final Defense Presentation.");
+    }
+}
